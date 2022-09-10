8057d822d8a266c8694d532f06980365e43ac246
Gergő Radeczki <g.radeczki@gmail.com>
2022-09-10 23:29:30 +0200

    Added primitives from Figma

--- frontend/utils/theme.config.ts
+++ frontend/utils/theme.config.ts
@@ -1,11 +1,35 @@
 // src/@chakra-ui/gatsby-plugin/theme.js
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
 
-const theme = {
-  fonts: {
-    heading: 'Archivo, sans-serif',
-    body: '"Space Grotesk", sans-serif'
-  }
+const config: ThemeConfig = {
+  useSystemColorMode: true,
+  initialColorMode: 'dark'
 }
 
-export default extendTheme(theme)
+const theme = extendTheme({
+  config,
+  fonts: {
+    // https://www.figma.com/file/JOHGaqs67K52ZSj32xF8Bd/Weboldal?node-id=12%3A1076
+    heading: 'Archivo, sans-serif',
+    body: 'Space Grotesk, sans-serif',
+    mono: 'Menlo, monospace'
+  },
+  colors: {
+    // https://www.figma.com/file/JOHGaqs67K52ZSj32xF8Bd/Weboldal?node-id=12%3A1075
+    simonyi_zold: '#63BC47',
+    dark: '#231F20',
+    light: '#F8F8F8',
+    white: '#FFFFFF',
+    simonyi_zold_halvany: '#63BC47',
+    simonyi_sarga: '#FFFF3C'
+  },
+  styles: {
+    global: () => ({
+      body: {
+        bg: 'dark'
+      }
+    })
+  }
+})
+
+export default theme
