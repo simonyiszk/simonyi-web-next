60f94de0a800f41bad2271787f45b378c7558758
Gergő Radeczki <g.radeczki@gmail.com>
2022-09-10 23:28:53 +0200

    Added icons from Figma

---
+++ frontend/components/icons/index.tsx
@@ -0,0 +1,11 @@
+import { ChevronIcon } from './Chevron'
+import { EmailIcon } from './Email'
+import { FacebookIcon } from './Facebook'
+import { GlobeIcon } from './Globe'
+import { IncreaseIcon } from './Increase'
+import { InstagramIcon } from './Instagram'
+import { LinkedInIcon } from './LinkedIn'
+import { PhoneIcon } from './Phone'
+import { YouTubeIcon } from './YouTube'
+
+export { ChevronIcon, EmailIcon, FacebookIcon, GlobeIcon, IncreaseIcon, InstagramIcon, LinkedInIcon, PhoneIcon, YouTubeIcon }
