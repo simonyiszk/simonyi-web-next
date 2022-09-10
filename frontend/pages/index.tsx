bb8b5e75185f1586bf5f72459094c75de1eaab67
Gergő Radeczki <g.radeczki@gmail.com>
2022-09-11 01:43:03 +0200

    Showcase Profile component on main page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@chakra-ui/react'
 import { NextSeo } from 'next-seo'
 import {
   ChevronIcon,
@@ -10,8 +11,51 @@ import {
   PhoneIcon,
   YouTubeIcon
 } from '../components/icons'
+import { Profile } from '../components/profile'
 import { SContainer } from '../components/_basics/SContainer'
 import { SLayout } from '../components/_basics/SLayout'
+
+function IconShowcase() {
+  return (
+    <Box display="flex" justifyContent="center">
+      <Box display="flex" gap={4}>
+        <ChevronIcon boxSize={8} fill="simonyi_zold" />
+        <EmailIcon boxSize={8} fill="simonyi_zold" />
+        <FacebookIcon boxSize={8} fill="simonyi_zold" />
+        <GlobeIcon boxSize={8} fill="simonyi_zold" />
+        <IncreaseIcon boxSize={8} fill="simonyi_zold" />
+        <InstagramIcon boxSize={8} fill="simonyi_zold" />
+        <LinkedInIcon boxSize={8} fill="simonyi_zold" />
+        <PhoneIcon boxSize={8} fill="simonyi_zold" />
+        <YouTubeIcon boxSize={8} fill="simonyi_zold" />
+      </Box>
+    </Box>
+  )
+}
+
+function ProfileShowcase() {
+  return (
+    <Box display="flex" justifyContent="center">
+      <Box display="flex" gap={4} flexWrap="wrap" justifyContent="center">
+        {[1, 2, 3, 4, 5].map((i) => (
+          <Profile
+            key={i}
+            name="Piller Trisztan"
+            title="Elnok"
+            profilePictureUrl="/profile.jpg"
+            socials={[
+              { socialIcon: 'email', url: 'email' },
+              { socialIcon: 'phone', url: 'phone' },
+              { socialIcon: 'facebook', url: 'facebook' },
+              { socialIcon: 'instagram', url: 'instagram' },
+              { socialIcon: 'linkedin', url: 'linkedin' }
+            ]}
+          />
+        ))}
+      </Box>
+    </Box>
+  )
+}
 
 export default function IndexPage() {
   return (
@@ -19,15 +63,8 @@ export default function IndexPage() {
       <NextSeo canonical="https://simonyi.bme.hu" />
       <SLayout>
         <SContainer>
-          <ChevronIcon boxSize="24" />
-          <EmailIcon boxSize="24" />
-          <FacebookIcon boxSize="24" />
-          <GlobeIcon boxSize="24" />
-          <IncreaseIcon boxSize="24" />
-          <InstagramIcon boxSize="24" />
-          <LinkedInIcon boxSize="24" />
-          <PhoneIcon boxSize="24" />
-          <YouTubeIcon boxSize="24" />
+          <IconShowcase />
+          <ProfileShowcase />
         </SContainer>
       </SLayout>
     </>
