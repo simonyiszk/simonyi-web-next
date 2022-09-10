83e22eb897f9c043969cff997879f9adce22e48e
Gergő Radeczki <g.radeczki@gmail.com>
2022-09-11 01:42:29 +0200

    Added new color

--- frontend/utils/theme.config.ts
+++ frontend/utils/theme.config.ts
@@ -18,6 +18,7 @@ const theme = extendTheme({
     // https://www.figma.com/file/JOHGaqs67K52ZSj32xF8Bd/Weboldal?node-id=12%3A1075
     simonyi_zold: '#63BC47',
     dark: '#231F20',
+    darkmode_regular: '#333333',
     light: '#F8F8F8',
     white: '#FFFFFF',
     simonyi_zold_halvany: '#63BC47',
