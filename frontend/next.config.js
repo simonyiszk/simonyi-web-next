5a452a6ac2257c303488ea2ac0c914a64daf245a
Gergo Radeczki <g.radeczki@gmail.com>
2022-12-10 22:45:11 +0100

    Updating to Next 13

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,7 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
-
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      /* development only config options here */
-    }
-  }
-
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    /* config options here */
-  }
-  return nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true
 }
+
+module.exports = nextConfig
