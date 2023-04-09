eb4ea119d90f311418b35b5d5a87db2fa866ed9a
Gergo Radeczki <g.radeczki@gmail.com>
2023-04-09 17:24:27 +0200

    Adding fontsource

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import { about, groups, images, profiles } from '~/utils';
 import HomePage from './home-page';
+import '@fontsource/space-grotesk/300.css';
+import '@fontsource/space-grotesk/400.css';
+import '@fontsource/space-grotesk/700.css';
+import '@fontsource/archivo/300.css';
+import '@fontsource/archivo/400.css';
+import '@fontsource/archivo/700.css';
 
 async function getData() {
   const result = await Promise.resolve('todo');
