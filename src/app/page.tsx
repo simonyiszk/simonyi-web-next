6c5cc3505eefc729da92af9a9c5e68a50575ccb4
Gergo Radeczki <g.radeczki@gmail.com>
2023-04-10 12:38:37 +0200

    Removing unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,6 @@ import '@fontsource/archivo/300.css';
 import '@fontsource/archivo/400.css';
 import '@fontsource/archivo/700.css';
 
-async function getData() {
-  const result = await Promise.resolve('todo');
-  return result;
-}
-
-export default async function Page() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const result = await getData();
+export default function Page() {
   return <HomePage aboutData={about} imagesData={images} presidencyData={profiles} studentGroupsData={groups} />;
 }
