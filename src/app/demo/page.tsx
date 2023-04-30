95a4b0f294e6195aa37e2392f61bc3048abf7aaf
Gergo Radeczki <g.radeczki@gmail.com>
2023-04-30 18:56:59 +0200

    Adding demo mdx page

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { Box } from '@chakra-ui/react';
+import HelloWorld from './demo.mdx';
+
+export default function Page() {
+  return (
+    <Box>
+      <HelloWorld />
+    </Box>
+  );
+}
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,3 +1,4 @@
+import { Link, Text } from '@chakra-ui/react';
 import type { MDXComponents } from 'mdx/types';
 
 // This file allows you to provide custom React components
@@ -6,10 +7,19 @@ import type { MDXComponents } from 'mdx/types';
 // other libraries.
 
 // This file is required to use MDX in `app` directory.
+// Example: https://github.com/vercel/next.js/tree/canary/examples/app-dir-mdx
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     // Allows customizing built-in components, e.g. to add styling.
     // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
+    h1: ({ children }) => <Text as="h1">{children}</Text>,
+    h2: ({ children }) => <Text as="h2">{children}</Text>,
+    h3: ({ children }) => <Text as="h3">{children}</Text>,
+    h4: ({ children }) => <Text as="h4">{children}</Text>,
+    h5: ({ children }) => <Text as="h5">{children}</Text>,
+    h6: ({ children }) => <Text as="h6">{children}</Text>,
+    p: ({ children }) => <Text as="p">{children}</Text>,
+    a: ({ children }) => <Link color="simonyi_zold">{children}</Link>,
     ...components
   };
 }
