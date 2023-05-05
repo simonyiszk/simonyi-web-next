4b57f4875a51b4449b722566245b2cf47465dd34
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-06 01:47:33 +0200

    Fixing broken install

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
- 
+
     // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
@@ -81,5 +81,5 @@ module.exports = {
     },
     darkMode: ['class', '[data-theme="dark"]']
   },
-  plugins: [],
+  plugins: []
 };
