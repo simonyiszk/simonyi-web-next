4bced15fb70a52187f776201620db6f9d30d9277
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-06 22:33:39 +0200

    Allow links to wrap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { about, groups, images, profiles } from '~/utils';
-import { HomeAbout, HomeGreeting, HomePresidency, HomeStudentGroups, HomeSubsites } from '~/components/app/home';
+import { HomeAbout, HomeGreeting, HomePresidency, HomeStudentGroups, HomeSubpages } from '~/components/app/home';
 import { HomeFooter } from '~/components';
 
 export default function Page() {
@@ -8,7 +8,7 @@ export default function Page() {
       <HomeGreeting />
       <div className="max-w-home mx-auto p-8 flex flex-col gap-[calc(80px+2rem)] pb-[calc(80px+2rem)]">
         <div />
-        <HomeSubsites />
+        <HomeSubpages />
         <HomeAbout about={about} images={images} />
         <HomeStudentGroups groups={groups} />
         <HomePresidency profiles={profiles} />
