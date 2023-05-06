2bf1bd0abac5260fead3f4b187649ba478670799
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-06 22:18:05 +0200

    Slimmer demo

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -4,7 +4,7 @@ import HelloWorld from './demo.mdx';
 export default function Page() {
   return (
     <SubpageLayout>
-      <div className="rounded-md bg-darkmode_regular p-4">
+      <div className="rounded-md bg-darkmode_regular p-4 max-w-3xl">
         <HelloWorld />
       </div>
     </SubpageLayout>
