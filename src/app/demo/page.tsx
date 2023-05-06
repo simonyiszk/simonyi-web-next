246aff29b2aca38f74c0223fdf5b30e6ca4865e9
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-06 16:16:38 +0200

    Refactoring demo page layout

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,11 +1,9 @@
-'use client';
-import { Box } from '@chakra-ui/react';
 import HelloWorld from './demo.mdx';
 
 export default function Page() {
   return (
-    <Box>
+    <div>
       <HelloWorld />
-    </Box>
+    </div>
   );
 }
