53cc206f640a29cdd01493be5da18945c1cd9518
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-06 16:25:52 +0200

    Fixing siteUrl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html data-theme="dark" lang="hu">
       <head>
-        <SEO title="Főoldal" siteUrl="https://simonyi-web-next.vercel.app" />
+        <SEO title="Főoldal" siteUrl="https://simonyi.bme.hu" />
       </head>
       <body className="bg-dark text-white text-opacity-[0.92]">
         <div className="flex flex-col justify-between min-h-[100svh]">
