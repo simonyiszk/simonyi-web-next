1f72cc10ff0db601958da94c7bc07bc48f4e94b1
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-06 22:13:42 +0200

    Max width and link to demo page

--- src/components/header/subpage-header.component.tsx
+++ src/components/header/subpage-header.component.tsx
@@ -3,14 +3,17 @@ import { SimonyiLogoIcon } from '../icons';
 
 function SubpageHeader() {
   return (
-    <div className="flex flex-row w-full p-4 gap-x-8 gap-y-4 bg-darkmode_regular justify-start align-middle items-center">
-      <div className="w-8 h-8">
-        <SimonyiLogoIcon />
+    <div className="w-full bg-darkmode_regular">
+      <div className="flex flex-row w-full max-w-home p-4 gap-x-8 gap-y-4 justify-start align-middle items-center m-auto">
+        <div className="w-8 h-8">
+          <SimonyiLogoIcon />
+        </div>
+        <NextLink href="/">Főoldal</NextLink>
+        <NextLink href="/blog">Blog</NextLink>
+        <NextLink href="/about">Rólunk</NextLink>
+        <NextLink href="/success-stories">Büszkeségeink</NextLink>
+        <NextLink href="/demo">demo</NextLink>
       </div>
-      <NextLink href="/">Főoldal</NextLink>
-      <NextLink href="/blog">Blog</NextLink>
-      <NextLink href="/about">Rólunk</NextLink>
-      <NextLink href="/success-stories">Büszkeségeink</NextLink>
     </div>
   );
 }
