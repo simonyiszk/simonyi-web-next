72687c1788816a7f0f0aa14e61622991fbdea028
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-06 16:20:37 +0200

    Refactoring `Header` and `Footer`

--- src/components/header/home-header.component.tsx
+++ src/components/header/home-header.component.tsx
@@ -1,21 +1,20 @@
 import Image from 'next/image';
-import { Box, Link } from '@chakra-ui/react';
 
 function HomeHeader() {
   return (
-    <Box display="flex" columnGap={16} rowGap={8} justifyContent="center" alignItems="center" flexWrap="wrap" p={8}>
-      <Link href="http://www.bme.hu/" target="_blank" height="50px" width="188px" position="relative">
+    <div className="flex gap-x-16 gap-y-8 justify-center items-center flex-wrap p-8">
+      <a className="h-[50px] w-[188px] relative" href="http://www.bme.hu/" target="_blank" rel="noreferrer">
         <Image src="/images/bme/bme.png" alt="Logo of BME" fill />
-      </Link>
-      <Link href="http://www.vik.bme.hu/" target="_blank" height="50px" width="50px" position="relative">
-        <Image src="/images/bme/vik.png" alt="Logo of BME VIK" fill style={{ width: '100%' }} />
-      </Link>
-      <Link href="https://svie.hu/" target="_blank" width={{ base: 'auto', md: '188px' }}>
-        <Box height="50px" width="160px" position="relative">
+      </a>
+      <a className="h-[50px] w-[50px] relative" href="http://www.vik.bme.hu/" target="_blank" rel="noreferrer">
+        <Image src="/images/bme/vik.png" alt="Logo of BME VIK" fill />
+      </a>
+      <a className="w-auto md:w-[188px]" href="https://svie.hu/" target="_blank" rel="noreferrer">
+        <div className="h-[50px] w-[160px] relative">
           <Image src="/images/bme/schonherz.png" alt="Logo of Schonherz" fill />
-        </Box>
-      </Link>
-    </Box>
+        </div>
+      </a>
+    </div>
   );
 }
 
