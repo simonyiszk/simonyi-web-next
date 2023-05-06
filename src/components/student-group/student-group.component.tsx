96c15fe4e14899d1faf9d133757990722b0c530f
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-06 16:21:54 +0200

    Refactoring groups

--- src/components/student-group/student-group.component.tsx
+++ src/components/student-group/student-group.component.tsx
@@ -1,87 +1,61 @@
+'use client';
+
 import React, { useState } from 'react';
 import Image from 'next/image';
-import { Box, Link, Text, useBreakpointValue } from '@chakra-ui/react';
 import { ChevronIcon, SocialIcon } from '../icons';
 import type { StudentGroupType } from '../../types';
 
 function StudentGroup({ name, logo, description, socials = [], isDense = false }: StudentGroupType & { isDense?: boolean }) {
   const [isOpen, setIsOpen] = useState(false);
-  const isMobile = useBreakpointValue({ base: true, md: false }) as boolean;
 
   const handleToggle = (e: React.MouseEvent) => {
     e.preventDefault();
-    if (!isMobile) return;
+    //if (!isMobile) return;
     setIsOpen(!isOpen);
   };
 
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      borderRadius="1rem"
-      gap="2rem"
-      justifyContent="space-between"
-      padding="1.25rem"
-      bg="darkmode_regular"
-      maxWidth={{ base: '100%', md: '45%', lg: '31%' }}
-    >
-      <Box width="100%" display="flex" flexDirection="column" alignItems="center" gap="2rem">
-        <Box
-          width="100%"
-          display="flex"
-          flexDirection="row"
-          alignItems="center"
-          gap="2rem"
-          justifyContent="space-between"
-          _hover={{ cursor: { base: 'pointer', md: 'auto' } }}
+    <div className="flex flex-col items-center justify-between rounded-[1rem] gap-8 p-5 bg-darkmode_regular max-w-full md:max-w-[45%] lg:max-w-[31%]">
+      <div className="w-full flex flex-col items-center gap-8">
+        <div
+          className="w-full flex flex-row items-center gap-8 justify-between hover:cursor-pointer md:hover:cursor-auto"
           onClick={(e) => handleToggle(e)}
         >
-          <Text as="h3" width="full" textAlign={{ base: 'left', md: 'center' }}>
-            {name}
-          </Text>
-          {isMobile && (
-            <Box p="0.5rem" transform={isOpen ? 'scaleY(-1)' : 'scaleY(1)'}>
-              <ChevronIcon fill={isOpen ? 'simonyi_sarga' : 'simonyi_zold'} />
-            </Box>
-          )}
-        </Box>
-        {(!isMobile || isOpen) && (
-          <>
-            <Box height="100px" position="relative" width="100%" maxWidth="250px">
-              <Image src={logo.url} alt={logo.alt} fill />
-            </Box>
-            <Box>
-              <Text>{description}</Text>
-            </Box>
-          </>
-        )}
-      </Box>
-      {(!isMobile || isOpen) && (
-        <Box
-          width="100%"
-          display="flex"
-          justifyContent={isDense && !isMobile ? 'space-evenly' : 'center'}
-          gap={isDense && !isMobile ? 'auto' : '1.25rem'}
-          flexWrap="wrap"
-        >
-          {socials.map((social, index) => (
-            <Link
-              href={social.link.url}
-              title={social.link.title}
-              target="_blank"
-              key={index}
-              px="13px"
-              py="6px"
-              bgColor="simonyi_zold"
-              borderRadius="6px"
-            >
-              <SocialIcon iconName={social.icon} props={{ boxSize: 4, fill: 'white' }} />
-            </Link>
-          ))}
-        </Box>
-      )}
-    </Box>
+          <h3 className="font-heading text-h3 w-full text-left md:text-center">{name}</h3>
+          <div className={`block md:hidden p-2 ${isOpen ? '-scale-y-100' : 'scale-y-100'}`}>
+            <ChevronIcon
+              className={`w-4 leading-6 fill-light align-middle inline-block ${isOpen ? 'fill-simonyi_sarga' : 'fill-simonyi_zold'}`}
+            />
+          </div>
+        </div>
+        <>
+          <div className={`${isOpen ? 'block' : 'hidden md:block'} relative h-[100px] w-full max-w-[250px]`}>
+            <Image src={logo.url} alt={logo.alt} fill />
+          </div>
+          <div className={`${isOpen ? 'block' : 'hidden md:block'}`}>
+            <p className="font-body">{description}</p>
+          </div>
+        </>
+      </div>
+      <div
+        className={`${isOpen ? 'flex' : 'hidden md:flex'} flex-wrap w-full justify-center ${isDense ? 'md:justify-evenly' : ''} gap-5 ${
+          isDense ? 'gap-5 md:gap-0' : ''
+        }`}
+      >
+        {socials.map((social, index) => (
+          <a
+            href={social.link.url}
+            title={social.link.title}
+            target="_blank"
+            key={index}
+            className="bg-simonyi_zold px-[13px] py-[6px] rounded-[6px]"
+            rel="noreferrer"
+          >
+            <SocialIcon iconName={social.icon} props={{ className: 'w-4 h-4 leading-4 fill-white inline-block align-middle' }} />
+          </a>
+        ))}
+      </div>
+    </div>
   );
 }
 
