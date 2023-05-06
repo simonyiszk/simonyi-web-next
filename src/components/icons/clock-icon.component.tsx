730a11cb2695ecfc8d649bcc977cf16dd53ac304
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-06 16:21:16 +0200

    Refactoring icons

--- src/components/icons/clock-icon.component.tsx
+++ src/components/icons/clock-icon.component.tsx
@@ -1,9 +1,9 @@
-import { createIcon } from '@chakra-ui/react';
-
-const ClockIcon = createIcon({
-  displayName: 'ClockIcon',
-  viewBox: '0 0 24 24',
-  path: <path d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-});
+function ClockIcon() {
+  return (
+    <svg viewBox="0 0 24 24">
+      <path d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+  );
+}
 
 export { ClockIcon };
