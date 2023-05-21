bde788eadf33c7567213bad61b7a2e96f9f4f361
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-21 18:36:56 +0200

    changing path

--- src/app/lib/get-posts.ts
+++ src/app/lib/get-posts.ts
@@ -5,13 +5,13 @@ import { cache } from 'react';
 import { PostType } from '~/types';
 
 const getPosts = cache(async () => {
-  const posts = await fs.readdir('./src/app/(subpages)/blog/posts/');
+  const posts = await fs.readdir(path.join(process.cwd(), 'src/app/(subpages)/blog/posts'));
 
   return Promise.all(
     posts
       .filter((file) => path.extname(file) === '.mdx')
       .map(async (file) => {
-        const filePath = `./src/app/(subpages)/blog/posts/${file}`;
+        const filePath = path.join(process.cwd(), `src/app/(subpages)/blog/posts/${file}`);
         const postContent = await fs.readFile(filePath, 'utf8');
         const { data, content } = matter(postContent);
 
