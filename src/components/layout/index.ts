85ba1101817bc4098b7b04fcd551fc97dac9e7c8
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-21 15:21:33 +0200

    Use metadata api

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,3 +1,4 @@
 import { SubpageLayout } from './subpage-layout.component';
+import { HomeLayout } from './home-layout.component';
 
-export { SubpageLayout };
+export { SubpageLayout, HomeLayout };
