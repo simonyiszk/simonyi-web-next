81f70c61fd1997d45c3815a0af75ac1ab125dab1
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-21 18:25:15 +0200

    Making posts look better

--- src/app/(subpages)/blog/page.tsx
+++ src/app/(subpages)/blog/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import Link from 'next/link';
 import { getPosts } from '~/app/lib/get-posts';
 
 export const metadata: Metadata = {
@@ -10,13 +11,18 @@ export default async function Page() {
 
   return (
     <>
-      <div className="flex flex-col p-4 rounded-md bg-darkmode_regular ">
-        <h1>Blog</h1>
-      </div>
-      <div>
+      <div className="flex flex-col gap-8 items-stretch w-3xl">
         {posts.map((post) => {
           if (post) {
-            return <div key={post.slug}>{post.title}</div>;
+            return (
+              <div key={post.slug} className="flex flex-col gap-2 p-4 rounded-md bg-darkmode_regular max-w-3xl">
+                <Link href={{ pathname: `blog/${post.slug}` }} className="hover:underline font-heading text-3xl">
+                  {post.title}
+                </Link>
+                <p className="font-body">{post.date?.toLocaleDateString()}</p>
+                <p className="font-body">{post.description} </p>
+              </div>
+            );
           }
         })}
       </div>
