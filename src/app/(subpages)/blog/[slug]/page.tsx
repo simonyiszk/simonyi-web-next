1be81ae95aff8889f7917f9687ada60715fa5fff
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-21 17:49:14 +0200

    Show title and date

--- src/app/(subpages)/blog/[slug]/page.tsx
+++ src/app/(subpages)/blog/[slug]/page.tsx
@@ -15,16 +15,13 @@ export default async function PostPage({
 
   if (!post) return notFound();
 
-  const { title, author, date } = post;
+  const { title, date } = post;
 
   return (
     <div className="rounded-md bg-darkmode_regular p-4 max-w-3xl">
-      <div>
-        <h1>{title}</h1>
-        <div>
-          <span>{author}</span>
-          <span>{date?.toDateString()}</span>
-        </div>
+      <div className="mb-8">
+        <h1 className="text-h1 text-5xl font-heading mb-4">{title}</h1>
+        <div className="font-body">{date?.toLocaleDateString()}</div>
       </div>
       {/* @ts-expect-error RSC */}
       <MDXRemote
