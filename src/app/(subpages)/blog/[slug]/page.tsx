cca681cb13a4b006b46b8c57c1da81d55ff81a66
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-28 14:51:32 +0200

    Styling

--- src/app/(subpages)/blog/[slug]/page.tsx
+++ src/app/(subpages)/blog/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import { MDXRemote } from 'next-mdx-remote/rsc';
+import Image from 'next/image';
 import { getPost } from '~/app/lib/get-posts';
 import { mdxComponents } from '~/mdx-components';
 
@@ -31,13 +32,18 @@ export default async function PostPage({
 
   if (!post) return notFound();
 
-  const { title, date } = post;
+  const { title, date, previewImage } = post;
 
   return (
     <div className="rounded-md bg-darkmode_regular p-4 max-w-3xl">
+      {previewImage && (
+        <div className="rounded-t-md relative h-80 -mx-4 -mt-4 mb-8">
+          <Image src={previewImage} alt={''} fill className="rounded-t-md object-cover" />
+        </div>
+      )}
       <div className="mb-8">
-        <h1 className="text-h1 text-5xl font-heading mb-4">{title}</h1>
-        <div className="font-body">{date?.toLocaleDateString()}</div>
+        {title && <h1 className="text-h1 text-5xl font-heading mb-4">{title}</h1>}
+        {date && <div className="font-body">{date.toLocaleDateString()}</div>}
       </div>
       {/* @ts-expect-error RSC */}
       <MDXRemote
--- src/app/(subpages)/layout.tsx
+++ src/app/(subpages)/layout.tsx
@@ -4,7 +4,7 @@ export default function SubpageLayout({ children }: { children: React.ReactNode 
   return (
     <div className="flex flex-col justify-between min-h-safe_screen gap-16">
       <SubpageHeader />
-      <div className="self-center p-4 max-w-home">{children}</div>
+      <div className="flex p-4 max-w-home self-center">{children}</div>
       <HomeFooter />
     </div>
   );
