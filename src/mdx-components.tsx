647ba30cd163455b69a9411a605c8a25d88d7339
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-28 14:49:11 +0200

    Making quote nicer

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -86,6 +86,19 @@ export const mdxComponents: MDXComponents = {
     <li className="font-body ml-2" {...props}>
       {children}
     </li>
+  ),
+  blockquote: ({ children, ...props }) => (
+    <>
+      <blockquote className="font-body border-l-4 border-simonyi_zold pl-4 mb-4 italic" {...props}>
+        <svg aria-hidden="true" className="w-10 h-10 text-simonyi_zold" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+            fill="currentColor"
+          />
+        </svg>
+        {children}
+      </blockquote>
+    </>
   )
 };
 
