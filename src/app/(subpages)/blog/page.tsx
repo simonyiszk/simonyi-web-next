81cc735d85e3818e050e4296ea8dafd22b9efba9
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-28 14:51:32 +0200

    Styling

--- src/app/(subpages)/blog/page.tsx
+++ src/app/(subpages)/blog/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import Image from 'next/image';
 import Link from 'next/link';
 import { getPosts } from '~/app/lib/get-posts';
 
@@ -11,19 +12,26 @@ export default async function Page() {
 
   return (
     <>
-      <div className="flex flex-col gap-8 items-stretch w-3xl">
+      <div className="flex flex-col gap-8 max-w-3xl">
         {posts.map((post) => {
-          if (post) {
-            return (
-              <div key={post.slug} className="flex flex-col gap-2 p-4 rounded-md bg-darkmode_regular max-w-3xl">
-                <Link href={`/blog/${post.slug}`} className="hover:underline font-heading text-3xl">
-                  {post.title}
-                </Link>
-                <p className="font-body">{post.date?.toLocaleDateString()}</p>
-                <p className="font-body">{post.description} </p>
+          return (
+            <div key={post.slug} className="flex flex-col sm:flex-row rounded-md">
+              <div className="hidden sm:block relative w-[100px] h-full">
+                {post.previewImage ? (
+                  <Image src={post.previewImage} alt={post.previewImageAlt || ''} fill className="rounded-l-md object-cover" />
+                ) : (
+                  <div className="rounded-l-md bg-darkmode_regular w-full h-full" />
+                )}
               </div>
-            );
-          }
+              <div className="flex flex-col gap-2 p-4 bg-darkmode_regular rounded-md sm:rounded-l-none sm:rounded-r-md flex-1">
+                <Link href={`/blog/${post.slug}`} className="hover:underline font-heading text-3xl">
+                  {post.title || post.slug}
+                </Link>
+                {post.date && <p className="font-body">{post.date.toLocaleDateString()}</p>}
+                {post.description && <p className="font-body">{post.description}</p>}
+              </div>
+            </div>
+          );
         })}
       </div>
     </>
