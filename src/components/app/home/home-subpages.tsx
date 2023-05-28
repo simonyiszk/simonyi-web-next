0fc6207d5e4cd8646ab32dcd3d27c18e7275eb8e
Gergo Radeczki <g.radeczki@gmail.com>
2023-05-28 14:53:24 +0200

    Link to Blog

--- src/components/app/home/home-subpages.tsx
+++ src/components/app/home/home-subpages.tsx
@@ -4,16 +4,9 @@ import { Button } from '~/components/button';
 export default function HomeSubPages() {
   return (
     <div className="flex flex-col">
-      <h1 className="text-h1 font-heading mb-8">Aloldalaink</h1>
       <div className="flex flex-row flex-wrap justify-evenly gap-4">
         <Link href="/blog">
           <Button>Blog</Button>
-        </Link>
-        <Link href="/about">
-          <Button>Rólunk</Button>
-        </Link>
-        <Link href="/success-stories">
-          <Button>Büszkeségeink</Button>
         </Link>
       </div>
     </div>
