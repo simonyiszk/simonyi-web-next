220a5c8612786fb8aa94ae2b7dec0fd58ae58e2b
Gergő Radeczki <g.radeczki@gmail.com>
2023-06-02 15:18:36 +0200

    Fixing layout

--- src/app/(subpages)/blog/page.tsx
+++ src/app/(subpages)/blog/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 import { getPosts } from '~/app/lib/get-posts';
+import { Post } from '~/components/app/blog/post';
 
 export const metadata: Metadata = {
   title: 'Blog'
@@ -9,6 +10,14 @@ export const metadata: Metadata = {
 
 export default async function Page() {
   const posts = await getPosts();
+
+  return (
+    <div className="flex-grow self-center p-4 flex flex-col gap-8 w-full max-w-3xl">
+      {posts.map((post) => {
+        return <Post key={post.slug} data={post} />;
+      })}
+    </div>
+  );
 
   return (
     <>
