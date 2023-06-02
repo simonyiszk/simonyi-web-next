8d318985ad34f41a32c069fd6f18e2acfb13ed89
Gergő Radeczki <g.radeczki@gmail.com>
2023-06-02 15:26:33 +0200

    Removing unused code

--- src/app/(subpages)/blog/page.tsx
+++ src/app/(subpages)/blog/page.tsx
@@ -1,6 +1,4 @@
 import { Metadata } from 'next';
-import Image from 'next/image';
-import Link from 'next/link';
 import { getPosts } from '~/app/lib/get-posts';
 import { Post } from '~/components/app/blog/post';
 
@@ -17,32 +15,5 @@ export default async function Page() {
         return <Post key={post.slug} data={post} />;
       })}
     </div>
-  );
-
-  return (
-    <>
-      <div className="flex flex-col gap-8 max-w-3xl">
-        {posts.map((post) => {
-          return (
-            <div key={post.slug} className="flex flex-col sm:flex-row rounded-md">
-              <div className="hidden sm:block relative w-[100px] h-full">
-                {post.previewImage ? (
-                  <Image src={post.previewImage} alt={post.previewImageAlt || ''} fill className="rounded-l-md object-cover" />
-                ) : (
-                  <div className="rounded-l-md bg-darkmode_regular w-full h-full" />
-                )}
-              </div>
-              <div className="flex flex-col gap-2 p-4 bg-darkmode_regular rounded-md sm:rounded-l-none sm:rounded-r-md flex-1">
-                <Link href={`/blog/${post.slug}`} className="hover:underline font-heading text-3xl">
-                  {post.title || post.slug}
-                </Link>
-                {post.date && <p className="font-body">{post.date.toLocaleDateString()}</p>}
-                {post.description && <p className="font-body">{post.description}</p>}
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </>
   );
 }
