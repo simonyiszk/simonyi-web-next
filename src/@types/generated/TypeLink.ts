3fb8821199c8824d920196f848549673577d83b3
Gergő Radeczki <g.radeczki@gmail.com>
2023-08-11 21:04:27 +0200

    no more type error

--- src/@types/generated/TypeLink.ts
+++ src/@types/generated/TypeLink.ts
@@ -1,12 +1,9 @@
 import type { Entry, EntryFields } from "contentful";
 
 export interface TypeLinkFields {
-    contentTypeId: 'link';
-    fields: {
-        url: EntryFields.Symbol;
-        title: EntryFields.Symbol;
-        text: EntryFields.Symbol;
-    }
+    url: EntryFields.Symbol;
+    title: EntryFields.Symbol;
+    text: EntryFields.Symbol;
 }
 
 export type TypeLink = Entry<TypeLinkFields>;
--- src/@types/generated/TypePost.ts
+++ src/@types/generated/TypePost.ts
@@ -1,23 +1,20 @@
 import type { Asset, Entry, EntryFields } from "contentful";
 
 export interface TypePostFields {
-    contentTypeId: 'post';
-    fields: {
-        slug: EntryFields.Symbol;
-        body: EntryFields.RichText;
-        title?: EntryFields.Symbol;
-        description?: EntryFields.Text;
-        date?: EntryFields.Date;
-        author?: EntryFields.Symbol;
-        authors?: EntryFields.Symbol[];
-        published?: EntryFields.Boolean;
-        hidden?: EntryFields.Boolean;
-        tags?: EntryFields.Symbol[];
-        previewImage?: Asset;
-        ogImage?: Asset;
-        ogImageWidth?: EntryFields.Integer;
-        ogImageHeight?: EntryFields.Integer;
-    }
+    slug: EntryFields.Symbol;
+    body: EntryFields.RichText;
+    title?: EntryFields.Symbol;
+    description?: EntryFields.Text;
+    date?: EntryFields.Date;
+    author?: EntryFields.Symbol;
+    authors?: EntryFields.Symbol[];
+    published?: EntryFields.Boolean;
+    hidden?: EntryFields.Boolean;
+    tags?: EntryFields.Symbol[];
+    previewImage?: Asset;
+    ogImage?: Asset;
+    ogImageWidth?: EntryFields.Integer;
+    ogImageHeight?: EntryFields.Integer;
 }
 
 export type TypePost = Entry<TypePostFields>;
