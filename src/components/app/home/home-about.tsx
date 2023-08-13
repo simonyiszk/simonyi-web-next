12ca28bdb6c1f0e7935599a8b1e05a32f148508d
Gergő Radeczki <g.radeczki@gmail.com>
2023-08-13 21:26:26 +0200

    Updating imports

--- src/components/app/home/home-about.tsx
+++ src/components/app/home/home-about.tsx
@@ -1,11 +1,14 @@
-import { AboutType, LightboxImage } from '~/types';
+import { AboutType, LightboxImage } from '~/@types';
+import { contentfulDocumentToReactComponents } from '~/utils';
 import HomeAboutImageBrowser from './home-about-image-browser';
 
 export default function HomeAbout({ about, images }: { about: AboutType; images: LightboxImage[] }) {
   return (
     <div className="flex flex-col">
       <h1 className="mb-8 text-h1 font-heading">{about.title}</h1>
+
       <div className="grid gap-16 grid-cols-1 lg:grid-cols-2">
+        {contentfulDocumentToReactComponents(about.description)}
         <HomeAboutImageBrowser lightboxImages={images} />
       </div>
     </div>
