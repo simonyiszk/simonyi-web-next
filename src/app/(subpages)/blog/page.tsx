66923619466884d8d819a9e52aa59611e5d7e7da
Gergő Radeczki <g.radeczki@gmail.com>
2023-08-13 21:26:08 +0200

    Query data from Contentful

--- src/app/(subpages)/blog/page.tsx
+++ src/app/(subpages)/blog/page.tsx
@@ -1,13 +1,21 @@
 import { Metadata } from 'next';
-import { getPosts } from '~/app/lib/get-posts';
 import { Post } from '~/components/app/blog/post';
+import { getPosts } from '~/utils/contentful';
+
+export const dynamic = 'force-dynamic';
 
 export const metadata: Metadata = {
   title: 'Blog'
 };
 
-export default async function Page() {
+async function getData() {
   const posts = await getPosts();
+
+  return { posts };
+}
+
+export default async function Page() {
+  const { posts } = await getData();
 
   return (
     <div className="flex-grow self-center p-4 flex flex-col gap-8 w-full max-w-3xl">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,28 @@
-import { about, groups, images, profiles } from '~/utils';
 import { HomeAbout, HomeGreeting, HomePresidency, HomeStudentGroups, HomeSubpages } from '~/components/app/home';
 import { HomeFooter } from '~/components';
+import { getAbout, getLightbox, getProfiles, getStudentGroups } from '~/utils/contentful';
 
-export default function Page() {
+export const dynamic = 'force-static';
+
+async function getData() {
+  const about = await getAbout();
+  const lightbox = await getLightbox();
+  const groups = await getStudentGroups();
+  const profiles = await getProfiles();
+
+  return { about, lightbox, groups, profiles };
+}
+
+export default async function Page() {
+  const { about, lightbox, groups, profiles } = await getData();
+
   return (
     <>
       <HomeGreeting />
       <div className="max-w-home mx-auto p-8 flex flex-col gap-[calc(80px+2rem)] pb-[calc(80px+2rem)]">
         <div />
         <HomeSubpages />
-        <HomeAbout about={about} images={images} />
+        <HomeAbout about={about} images={lightbox} />
         <HomeStudentGroups groups={groups} />
         <HomePresidency profiles={profiles} />
       </div>
