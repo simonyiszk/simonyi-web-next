90c91b4bc9df80d26b8e0aebc5b59c7900b7f8d1
Gergő Radeczki <g.radeczki@gmail.com>
2023-08-15 01:45:14 +0200

    Does this fix the issue?

--- src/utils/contentful.ts
+++ src/utils/contentful.ts
@@ -13,24 +13,10 @@ import { defaults } from '.';
 
 export const revalidate = false; // only revalidate when redeployed
 
-const accessToken =
-  process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-    ? process.env.CONTENTFUL_ACCESS_TOKEN ?? 'Error no access token'
-    : process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
-    ? process.env.CONTENTFUL_ACCESS_TOKEN ?? 'Error no access token'
-    : process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN ?? 'Error no access token';
-
-const host =
-  process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-    ? 'cdn.contentful.com'
-    : process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
-    ? 'cdn.contentful.com'
-    : 'preview.contentful.com';
-
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID ?? 'No space ID',
-  accessToken,
-  host,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN ?? 'No access token',
+  host: 'cdn.contentful.com',
   environment: process.env.CONTENTFUL_ENVIRONMENT
 });
 
