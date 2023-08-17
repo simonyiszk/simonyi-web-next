b61c255a724ffbe89799e902a75621c22240f2dc
Gergo Radeczki <g.radeczki@gmail.com>
2023-08-17 18:05:52 +0200

    Implementing next-intl

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,45 +1,15 @@
-import { NextResponse } from 'next/server';
-import { match } from '@formatjs/intl-localematcher';
-import Negotiator from 'negotiator';
-import { Locale } from './utils';
-import type { NextRequest } from 'next/server';
+import createMiddleware from 'next-intl/middleware';
 
-const locales: Locale[] = ['hu', 'en'];
-const defaultLocale: Locale = 'en';
+export default createMiddleware({
+  // A list of all locales that are supported
+  locales: ['en', 'hu'],
 
-// Get the preferred locale, similar to above or using a library
-function getLocale(request: NextRequest) {
-  const headers = request.headers;
-  const languages = new Negotiator({
-    headers: {
-      'accept-language': headers.get('accept-language') || 'en-US'
-    }
-  }).languages();
-
-  return match(languages, locales, defaultLocale); // -> 'en'
-}
-
-export function middleware(request: NextRequest) {
-  // Check if there is any supported locale in the pathname
-  const pathname = request.nextUrl.pathname;
-
-  const pathnameIsMissingLocale = locales.every((locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`);
-
-  // Redirect if there is no locale
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request);
-
-    // e.g. incoming request is /products
-    // The new URL is now /en-US/products
-    return NextResponse.redirect(new URL(`/${locale}/${pathname}`, request.url));
-  }
-}
+  // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
+  defaultLocale: 'hu'
+});
 
 export const config = {
-  matcher: [
-    // Skip all internal paths (_next)
-    '/((?!api|static|.*\\..*|_next).*)'
-    // Optional: only run on root (/) URL
-    // '/'
-  ]
+  // Skip all paths that should not be internationalized. This example skips the
+  // folders "api", "_next" and all files with an extension (e.g. favicon.ico)
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,3 @@ export * from './functions';
 export * from './defaults';
 export * from './contentful';
 export * from './locales';
-export * from './dictionaries';
