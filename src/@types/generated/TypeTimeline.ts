3416ad70e5221b3076611c335eae181bdc1ac1c0
Gergő Radeczki <g.radeczki@gmail.com>
2023-09-18 21:45:33 +0200

    Keeping up with type updates

---
+++ src/@types/generated/TypeTimeline.ts
@@ -0,0 +1,10 @@
+import type { ChainModifiers, Entry, EntryFieldTypes, EntrySkeletonType, LocaleCode } from "contentful";
+
+export interface TypeTimelineFields {
+    year: EntryFieldTypes.Integer;
+    description: EntryFieldTypes.RichText;
+    important: EntryFieldTypes.Boolean;
+}
+
+export type TypeTimelineSkeleton = EntrySkeletonType<TypeTimelineFields, "timeline">;
+export type TypeTimeline<Modifiers extends ChainModifiers, Locales extends LocaleCode> = Entry<TypeTimelineSkeleton, Modifiers, Locales>;
