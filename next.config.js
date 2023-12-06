37ab8ccbbac7f89146f74b3ae4c8aedbea38c3eb
Gergő Radeczki <g.radeczki@gmail.com>
2023-12-06 23:40:12 +0100

    Updating linting

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['ts', 'tsx', 'md'],
+  pageExtensions: ["ts", "tsx", "md"],
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['warp.sch.bme.hu', 'images.ctfassets.net', 'cdn.contentful.com', 'preview.contentful.com']
-  }
+    domains: ["warp.sch.bme.hu", "images.ctfassets.net", "cdn.contentful.com", "preview.contentful.com"],
+  },
 };
 
 module.exports = nextConfig;
