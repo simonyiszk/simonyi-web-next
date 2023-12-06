55518bd9a2c28d30143f4eee52a8ef1de792d771
Gergő Radeczki <g.radeczki@gmail.com>
2023-12-07 00:50:47 +0100

    Hungarians are special :)

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,9 +6,18 @@ export default createMiddleware({
 
   // Used when no locale matches
   defaultLocale: "hu",
+  localePrefix: "as-needed",
 });
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ["/", "/(en|hu)/:path*"],
+  // Matcher entries are linked with a logical "or", therefore
+  // if one of them matches, the middleware will be invoked.
+  matcher: [
+    // Match all pathnames except for
+    // - … if they start with `/api`, `/_next` or `/_vercel`
+    // - … the ones containing a dot (e.g. `favicon.ico`)
+    "/((?!api|_next|_vercel|.*\\..*).*)",
+    // Match all pathnames within `/users`, optionally with a locale prefix
+    "/([\\w-]+)?/users/(.+)",
+  ],
 };
