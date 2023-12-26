35b68f9d342da95de7b699ace965b108dc3c6fdd
Gergő Radeczki <g.radeczki@gmail.com>
2023-12-26 19:18:34 +0100

    Fix translation

--- src/components/app/home/home-student-groups.tsx
+++ src/components/app/home/home-student-groups.tsx
@@ -1,14 +1,11 @@
-import { useTranslations } from "next-intl";
 import { StudentGroup, Typography } from "~/components";
 import { CurrentStudnetGroupsType } from "~/@types";
 
 export default function HomeStudentGroups({ currentStudentGroups }: { currentStudentGroups: CurrentStudnetGroupsType }) {
-  const t = useTranslations("home");
-
   return (
     <div className="flex flex-col gap-8">
       <div className="self-center md:self-start">
-        <Typography as="h1" variant="h1">{t("studentGroups")}</Typography>
+        <Typography as="h1" variant="h1">{currentStudentGroups.title}</Typography>
       </div>
       <div className="flex w-full flex-col flex-wrap justify-center gap-8 self-center md:flex-row">
         {currentStudentGroups.studentGroups.map((group, index) => (
