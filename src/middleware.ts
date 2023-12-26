d0ad14ebb21870d45959b8b689613d73f8d15cfd
Gergő Radeczki <g.radeczki@gmail.com>
2023-12-26 19:44:28 +0100

    Reverting

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,5 +10,14 @@ export default createMiddleware({
 });
 
 export const config = {
-  matcher: ["/", "/(hu|en)/:path*"],
+  // Matcher entries are linked with a logical "or", therefore
+  // if one of them matches, the middleware will be invoked.
+  matcher: [
+    // Match all pathnames except for
+    // - … if they start with `/api`, `/_next` or `/_vercel`
+    // - … the ones containing a dot (e.g. `favicon.ico`)
+    "/((?!api|_next|_vercel|.*\\..*).*)",
+    // However, match all pathnames within `/users`, optionally with a locale prefix
+    "/([\\w-]+)?/users/(.+)",
+  ],
 };
