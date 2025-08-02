b480e6bf537f3372738e6167e4dec0d5f427cfc7
Gergő Radeczki <g.radeczki@gmail.com>
2025-08-02 11:21:54 +0200

    new `Button`

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,20 +1,46 @@
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
+
 import { cn } from "~/utils/cn"
 
-type ButtonProps = React.DetailedHTMLProps<
-  React.ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
->
+const buttonVariants = cva(
+  "inline-flex h-10 w-32 items-center justify-center rounded-md border-2 border-primary bg-white/15 px-4 font-body font-semibold transition duration-200 ease-in-out hover:bg-primary",
+  {
+    variants: {
+      variant: {
+        default:
+          "",
+      },
+      size: {
+        default: "",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
 
-export function Button({ className, children, ...rest }: ButtonProps) {
+function Button({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+  }) {
+  const Comp = asChild ? Slot : "button"
+
   return (
-    <button
-      className={cn(
-        "inline-flex h-10 w-32 items-center justify-center rounded-md border-2 border-primary bg-white/15 px-4 font-body font-semibold transition duration-200 ease-in-out hover:bg-primary",
-        className,
-      )}
-      {...rest}
-    >
-      {children}
-    </button>
+    <Comp
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
   )
 }
+
+export {Button, buttonVariants}
