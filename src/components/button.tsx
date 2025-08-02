c447101a424cdfd52b9b11efe0ca2a4a8b08c119
Gergő Radeczki <g.radeczki@gmail.com>
2025-08-02 22:01:36 +0200

    Refactor buttons

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,39 +4,27 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "~/utils/cn"
 
 const buttonVariants = cva(
-  `
-    m-1 inline-flex shrink-0 cursor-pointer items-center justify-center gap-2
-    rounded-md font-body font-semibold whitespace-nowrap transition-all
-    duration-200 ease-in-out outline-none
-    disabled:pointer-events-none
-  `,
+  `m-1 inline-flex shrink-0 cursor-pointer items-center justify-center gap-2 rounded-md font-body font-semibold whitespace-nowrap transition-all duration-200 ease-in-out outline-none disabled:pointer-events-none`,
   {
     variants: {
       variant: {
-        default: `
-          bg-primary ring-2 ring-primary
-          hover:bg-primary-400 hover:ring-4 hover:ring-primary-400
-          active:bg-primary-600 active:ring-2 active:ring-primary-600
-        `,
-        outline: `
-          bg-white/15 ring-2 ring-primary
-          hover:bg-primary-400 hover:ring-4 hover:ring-primary-400
-          active:bg-primary-600 active:ring-2 active:ring-primary-600
-        `,
-        transparent: `
-          bg-white/15 ring-2 ring-white/15
-          hover:bg-primary-400 hover:ring-4 hover:ring-primary-400
-          active:bg-primary-600 active:ring-2 active:ring-primary-600
-        `,
+        default: `bg-primary ring-2 ring-primary hover:bg-primary-400 hover:ring-4 hover:ring-primary-400 active:bg-primary-600 active:ring-2 active:ring-primary-600`,
+        outline: `bg-white/15 ring-2 ring-primary hover:bg-primary-400 hover:ring-4 hover:ring-primary-400 active:bg-primary-600 active:ring-2 active:ring-primary-600`,
+        transparent: `bg-white/15 ring-2 ring-white/15 hover:bg-primary-400 hover:ring-4 hover:ring-primary-400 active:bg-primary-600 active:ring-2 active:ring-primary-600`,
       },
       size: {
         default: "min-h-10 min-w-32 px-4",
         icon: "size-8",
       },
+      isActive: {
+        true: "bg-primary",
+        false: "",
+      },
     },
     defaultVariants: {
       variant: "default",
       size: "default",
+      isActive: false,
     },
   },
 )
@@ -45,6 +33,7 @@ function Button({
   className,
   variant,
   size,
+  isActive,
   asChild = false,
   ...props
 }: React.ComponentProps<"button"> &
@@ -56,7 +45,7 @@ function Button({
   return (
     <Comp
       data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(buttonVariants({ variant, size, isActive, className }))}
       {...props}
     />
   )
