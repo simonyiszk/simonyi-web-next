86aa111515ec918f0f98964601d1820a09d8f2ed
Trisztán Piller <60111772+triszt4n@users.noreply.github.com>
2026-09-10 00:37:19 +0000

    feat: new logos

--- src/components/bme-logos.tsx
+++ src/components/bme-logos.tsx
@@ -16,7 +16,7 @@ export function BmeLogos() {
         <Image
           src="/images/bme/bme.png"
           alt={t("bmeAlt")}
-          width={188}
+          width={242}
           height={50}
         />
       </a>
@@ -30,7 +30,7 @@ export function BmeLogos() {
         <Image
           src="/images/bme/vik.png"
           alt={t("vikAlt")}
-          width={50}
+          width={195}
           height={50}
         />
       </a>
@@ -45,6 +45,48 @@ export function BmeLogos() {
           src="/images/bme/schonherz.png"
           alt={t("schonherzAlt")}
           width={160}
+          height={50}
+        />
+      </a>
+      <a
+        className="relative h-[50px] w-[188px] drop-shadow-sm drop-shadow-black transition-all duration-200 hover:opacity-75"
+        title={t("ujbudaAlt")}
+        href="https://ujbuda.hu/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image
+          src="/images/bme/ujbuda.png"
+          alt={t("ujbudaAlt")}
+          width={171}
+          height={50}
+        />
+      </a>
+      <a
+        className="relative h-[50px] w-[188px] drop-shadow-sm drop-shadow-black transition-all duration-200 hover:opacity-75"
+        title={t("miniszteriumAlt")}
+        href="https://kormany.hu/kormanyzat/oktatasi-es-gyermekugyi-miniszterium"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image
+          src="/images/bme/miniszterium.png"
+          alt={t("miniszteriumAlt")}
+          width={109}
+          height={50}
+        />
+      </a>
+      <a
+        className="relative h-[50px] w-[188px] drop-shadow-sm drop-shadow-black transition-all duration-200 hover:opacity-75"
+        title={t("ntpAlt")}
+        href="https://emet.gov.hu/kategoria/kiemelt-kategoriak/palyazatok/aktualis-felhivasok/nemzeti-tehetseg-program/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image
+          src="/images/bme/ntp.png"
+          alt={t("ntpAlt")}
+          width={140}
           height={50}
         />
       </a>
